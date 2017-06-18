9c3233d39a204461fbe0f77c6ea1a0450db234d6
Maksim Byakov <bms1200@gmail.com>
2017-06-18 20:17:24 +0300

    New php

--- php/chat_script.php
+++ php/chat_script.php
@@ -1,13 +1,24 @@
 <?php
-$user = $_POST['user'];
+session_start();
+$user = $_SESSION['user'];
 $message = $_POST['message'];
 $time = date('H:i:s');
 
 $file = "../data/messages.json";
+$content = json_decode(file_get_contents($file), true);
 
-$content = json_decode(file_get_contents($file), true) ?:
-	[$user => $message];
+if (!empty($message)) {
+	$content[] = array('time' => $time, 'user' => $user, 'message'  => $message);
 
-$pretty = json_encode($content, JSON_PRETTY_PRINT);
-file_put_contents($file, $pretty);
+	$new_content = array();
+	$i = 0;
+	foreach($content as $key => $id) {
+		$new_id = $id;
+		$new_id['id'] = $i;
+		$new_content[$id['user'] ] = $new_id;
+		$i++;
+	}
+	file_put_contents($file, json_encode($new_content, JSON_PRETTY_PRINT),FILE_APPEND);
+}
+echo (json_encode($new_content));
 ?>
--- php/login_script.php
+++ php/login_script.php
@@ -1,20 +1,42 @@
 <?php
-$name = $_POST['user'];
+session_start();
+$name     = $_POST['user'];
 $password = $_POST['pass'];
-if(!isset($name, $password) || empty($name) || empty($password)){
+$_SESSION['user'] = $name;
+$_SESSION['pass'] = $password;
+
+if( !isset($name, $password) || empty($name) || empty($password) ){
 	http_response_code(400);
 	die();
 }
-$file = "../data/users.json";
-$users = json_decode(file_get_contents($file), true);
 
-if(isset($users[$name])) {
-	if($users[$name] != $password){
+$file = "../data/users.json";
+
+$json_array = json_decode(file_get_contents($file), true);
+
+$users = array();
+
+if(is_array($json_array) ) {
+	$users = $json_array;
+}
+
+if(isset($users[$name]) ) {
+	if( $users[$name]['pass'] !== $password ){
 		http_response_code(400);
 	}
 } 
 else {
-	$users[$name] = $password;
-	file_put_contents($file, json_encode($users, JSON_PRETTY_PRINT));
+	$users[] = array('name' => $name, 'pass' => $password);
+
+	$new_users = array();
+	$i = 0;
+	foreach($users as $key => $user) {
+		$new_user = $user;
+		$new_user['id'] = $i;
+		$new_users[ $user['name'] ] = $new_user;
+		$i++;
+	}
+
+	file_put_contents($file, json_encode($new_users, JSON_PRETTY_PRINT));
 }
 ?>
