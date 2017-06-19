7443883a2a362c5424c0e3e908d0a0510ba7469b
Maksim Byakov <bms1200@gmail.com>
2017-06-19 11:48:57 +0300

    new chat script

--- ajax/chat.js
+++ ajax/chat.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-	var name = $('#username').val();
-	$.ajax({
-		type : 'POST',
-		url  : 'php/chat_script.php',
-		data : {
-			user:name
-		},
-		success: function(a) {
-			alert(a);
-		}
+	$('#submit').click(function(){
+		var name = $('#username').val();
+		$.ajax({
+			type : 'POST',
+			url  : 'php/chat_script.php',
+			data : {
+				user:name
+			},
+			success: function(a) {
+				alert(a);
+			}
+		});
 	});
 });
